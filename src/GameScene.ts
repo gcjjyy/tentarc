d5bc275479b068d8279be7bbfa68704bd00e537e
Gyeongcheol Jang <gcjjyy@gmail.com>
2018-09-24 02:00:16 +0900

    RPG Prototype

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -3,19 +3,28 @@ import Scene from '@/engine/Scene';
 import Sprite from '@/engine/Sprite';
 import TileMap from '@/engine/TileMap';
 import TiledJsonLoader from '@/engine/TiledJsonLoader';
+import SpriteJsonLoader from '@/engine/SpriteJsonLoader';
 
 export default class GameScene extends Scene {
     private keyDown: boolean[] = [];
     private map: TileMap | null = null;
+    private sprite: Sprite | null = null;
 
     constructor(game: Game) {
         super(game);
 
-        TiledJsonLoader.load(game, './map/sandbox.json', (map: TileMap | null): void => {
+        TiledJsonLoader.load(game, './tilemap.json', (map: TileMap | null): void => {
             if (map) {
                 this.addGameObject(map);
                 this.map = map;
             }
+
+            SpriteJsonLoader.load(this.game, 'hero.json', (sprite: Sprite | null): void => {
+                this.sprite = sprite;
+                if (sprite) {
+                    this.addGameObject(sprite).setPosition(game.designedWidth / 2, game.designedHeight / 2);
+                }
+            });
         });
     }
 
@@ -28,15 +37,19 @@ export default class GameScene extends Scene {
     }
 
     public onUpdate = (dt: number): void => {
-        if (this.map) {
+        if (this.map && this.sprite) {
             if (this.keyDown[37]) {
-                this.map.setX(this.map.getX() - 5);
+                this.map.setX(this.map.getX() + 2);
+                this.sprite.setAnimation(3);
             } else if (this.keyDown[38]) {
-                this.map.setY(this.map.getY() - 5);
+                this.map.setY(this.map.getY() + 2);
+                this.sprite.setAnimation(2);
             } else if (this.keyDown[39]) {
-                this.map.setX(this.map.getX() + 5);
+                this.map.setX(this.map.getX() - 2);
+                this.sprite.setAnimation(1);
             } else if (this.keyDown[40]) {
-                this.map.setY(this.map.getY() + 5);
+                this.map.setY(this.map.getY() - 2);
+                this.sprite.setAnimation(0);
             }
         }
     }
--- src/LogoScene.ts
+++ src/LogoScene.ts
@@ -6,7 +6,6 @@ import DosFont from '@/engine/DosFont';
 import Sprite from '@/engine/Sprite';
 import Text from '@/engine/Text';
 import GameScene from '@/GameScene';
-import SpriteJsonLoader from '@/engine/SpriteJsonLoader';
 import Animation from '@/engine/Animation';
 import Frame from '@/engine/Frame';
 
@@ -28,28 +27,28 @@ export default class LogoScene extends Scene {
 
     public onShow = (): void => {
         console.log('LogoScene Show');
-/*
+
         this.sprite = new Sprite(this.chr);
 
-        const anim1 = new FrameAnimation('walk-down', 1);
+        const anim1 = new Animation('walk-down', 1);
         anim1.addFrame(new Frame(0, 0, 16, 32));
         anim1.addFrame(new Frame(16, 0, 16, 32));
         anim1.addFrame(new Frame(32, 0, 16, 32));
         anim1.addFrame(new Frame(48, 0, 16, 32));
 
-        const anim2 = new FrameAnimation('walk-right', 1);
+        const anim2 = new Animation('walk-right', 1);
         anim2.addFrame(new Frame(0, 32, 16, 32));
         anim2.addFrame(new Frame(16, 32, 16, 32));
         anim2.addFrame(new Frame(32, 32, 16, 32));
         anim2.addFrame(new Frame(48, 32, 16, 32));
 
-        const anim3 = new FrameAnimation('walk-up', 1);
+        const anim3 = new Animation('walk-up', 1);
         anim3.addFrame(new Frame(0, 64, 16, 32));
         anim3.addFrame(new Frame(16, 64, 16, 32));
         anim3.addFrame(new Frame(32, 64, 16, 32));
         anim3.addFrame(new Frame(48, 64, 16, 32));
 
-        const anim4 = new FrameAnimation('walk-left', 1);
+        const anim4 = new Animation('walk-left', 1);
         anim4.addFrame(new Frame(0, 96, 16, 32));
         anim4.addFrame(new Frame(16, 96, 16, 32));
         anim4.addFrame(new Frame(32, 96, 16, 32));
@@ -60,15 +59,9 @@ export default class LogoScene extends Scene {
             this.sprite.addAnimation(anim2);
             this.sprite.addAnimation(anim3);
             this.sprite.addAnimation(anim4);
-        }
-*/
 
-        SpriteJsonLoader.load(this.game, 'hero.json', (sprite: Sprite | null): void => {
-            this.sprite = sprite;
-            if (sprite) {
-                this.addGameObject(sprite).setPosition(64, 64);
-            }
-        });
+            this.addGameObject(this.sprite).setPosition(64, 64);
+        }
 
         this.addGameObject(new Text(this.fnt, 'ABCD1234ab\ncd!!')).setPosition(0, 16);
         this.addGameObject(new Text(this.fnt, 'Hello~! 반가워!', 48, 16)).setPosition(0, 48);
