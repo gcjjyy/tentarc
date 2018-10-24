ef2c2f2a97533c309d8cb383d463a1e09292898a
Gyeongcheol Jang <gcjjyy@gmail.com>
2018-10-24 22:29:04 +0900

    Add Tentarc.ts

---
+++ Tentarc.ts
@@ -0,0 +1,45 @@
+import Animation from './Animation';
+import Audio from './Audio';
+import DosFont from './DosFont';
+import Font from './Font';
+import Frame from './Frame';
+import Image from './Image';
+import ImageFont from './ImageFont';
+import LocalFileLoader from './LocalFileLoader';
+import Plane from './Plane';
+import Rect from './Rect';
+import Resource from './Resource';
+import Scene from './Scene';
+import SceneObject from './SceneObject';
+import Screen from './Screen';
+import Sprite from './Sprite';
+import SpriteJsonLoader from './SpriteJsonLoader';
+import Text from './Text';
+import TileMap from './TileMap';
+import TileMapLayer from './TileMapLayer';
+import TileSet from './TileSet';
+import TiledJsonLoader from './TiledJsonLoader';
+
+export {
+    Animation,
+    Audio,
+    DosFont,
+    Font,
+    Frame,
+    Image,
+    ImageFont,
+    LocalFileLoader,
+    Plane,
+    Rect,
+    Resource,
+    Scene,
+    SceneObject,
+    Screen,
+    Sprite,
+    SpriteJsonLoader,
+    Text,
+    TileMap,
+    TileMapLayer,
+    TileSet,
+    TiledJsonLoader,
+};
